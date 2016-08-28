a60ff5d1820046cb442b7df31223d7b73d85894e
dchasepdx <dchasepdx@gmail.com>
2016-08-27 23:36:12 -0700

    add plenty of comments to main.js. added if statements to prevent errors if LS was empty

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var brew = document.getElementById('brew');
 var changes = document.getElementById('changes');
 for(i = 0; i<flavor.length; i++) {
   flavor[i].addEventListener('click', function() {
-
+    //sets brew and changes <span> to the correct flavor and recommendation
     brew.textContent = this.id;
     switch(this.id) {
       case "strong":
@@ -44,26 +44,30 @@ for(i = 0; i<flavor.length; i++) {
 }
 
 //functions to hide recommendation on load and show on click
-//function hide() {
-  //document.getElementById('show').id = 'hide';
-//}
+/*function hide() {
+  document.getElementById('show').id = 'hide';
+} un needed code. Delete after review*/
+for(i = 0; i<flavor.length; i++) {
+  flavor[i].addEventListener('click', show);
+}
+
 function show() {
   if (document.getElementById('hide')) {
     document.getElementById('hide').id = 'show';
   }
 
 }
-//window.onload = hide;
-for(i = 0; i<flavor.length; i++) {
-  flavor[i].addEventListener('click', show);
-}
+//window.onload = hide; un needed code, delete after review
 
-//functions for saving to localStorage and drop down menu
+
+/*functions for saving to localStorage and drop down menu as well as hide function.
+Needed to be in windows.onload becuase many dynamically created elements weren't
+loaded before functions ran resulting in null and undefined variables*/
 window.onload = function() {
-  function hide() {
+  function hide() { //hide function is unnecessary.
     document.getElementById('show').id = 'hide';
   }
-
+  //localStorage
   submit.onclick = save;
   function save() {
     var submit = document.getElementById('submit');
@@ -71,42 +75,54 @@ window.onload = function() {
     var dose = document.getElementById("dose").value;
     var waterWeight = document.getElementById("waterWeight").value;
     var time = document.getElementById("time").value;
-    var values = [dose, waterWeight, time];
+    var values = [dose, waterWeight, time];//saved value as array to access by index
     localStorage.setItem(origin, JSON.stringify(values));
 
   }
+  //creating dropdown menu. Had to be in onload function because localStorage was loading after variables were assigned
+
   function createDrop() {
 
     var select = document.getElementById('select');
-    var keys = [];
+    var keys = [];//set keys to array for looping and attaching <option> tag.
 
-    for (var i = 0; i < localStorage.length; i++) {
+    for (var i = 0; i < localStorage.length; i++) { //loop to push LS keys to array
       keys.push(localStorage.key(i));
       }
-      for (var val in keys) {
+      for (var val in keys) {     //loop to create <option> tag
         var newDrop = document.createElement('option');
         newDrop.innerText = keys[val];
         newDrop.setAttribute('value', localStorage.key(val));
         select.appendChild(newDrop);
         }
       }
+      if(localStorage.length > 0){
       createDrop();
-      hide();
+    } //runs the function after the page has loaded ensuring variables are assigned correctly.
+      //hide(); unnecessary, set class to hide in html
       //functions for dispalying saved recipes
       var select = document.getElementById('select');
       var originH3 = document.getElementById('originSave');
       var doseH3 = document.getElementById('doseSave');
       var waterWeightH3 = document.getElementById('waterWeightSave');
       var timeH3 = document.getElementById('timeSave');
-
-
-      select.onchange = function() {
-        var choice = document.getElementById('select').value;
-        originH3.textContent = "Origin: " + choice;
-        var values = JSON.parse(localStorage.getItem(choice));
+      var values = JSON.parse(localStorage.getItem(select.value));
+      //sets a saved brew on page load. Important if there's only 1 saved brew. Cannot trigger change event if there's only one option in the menu. Try and think of less redundant way to do this. same code exists in the onchange event handler.
+      if (localStorage.length > 0) {
+        originH3.textContent = "Origin: " + document.getElementById('select').value;
         doseH3.textContent = "Dose: " + values[0];
         waterWeightH3.textContent = "Water weight: " + values[1];
         timeH3.textContent = "Time: " + values[2];
+
+      //function for changing saved brew text content on change of dropdown. needs to be in onload function for proper variable assignment.
+        select.onchange = function() {
+          var choice = document.getElementById('select').value;
+          var values = JSON.parse(localStorage.getItem(choice));
+          originH3.textContent = "Origin: " + choice;
+          doseH3.textContent = "Dose: " + values[0];
+          waterWeightH3.textContent = "Water weight: " + values[1];
+          timeH3.textContent = "Time: " + values[2];
+        }
       }
 
     }
